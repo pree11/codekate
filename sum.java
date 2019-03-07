862108e0e4e6b8f82375558ab63099210c95ab78
pree11 <47377140+pree11@users.noreply.github.com>
2019-03-07 05:05:50 -0800

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,12 @@
+pimport java.util.Scanner;
+
+public class SumofAP 
+{
+public static void main(String[] arg)
+    {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter first term, common difference and n (number of terms)");
+    int a=sc.nextInt(),d=sc.nextInt(),n=sc.nextInt();
+    System.out.print("Sum: "+n*(2*a+(n-1)*d)/2);
+    }
+}
